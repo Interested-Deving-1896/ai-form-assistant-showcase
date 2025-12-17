acd34de5bacc097120939eced849d019bc473bdb
Csaky <tim.csaky@gov.bc.ca>
2025-12-17 15:33:26 -0800

    scaffold

---
+++ frontend/src/views/DeveloperView.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
+import { useAuthStore, useConfigStore } from '@/store';
+
+// Store
+const { getAccessToken, getProfile } = storeToRefs(useAuthStore());
+const { getConfig } = storeToRefs(useConfigStore());
+</script>
+
+<template>
+  <h2>Developer</h2>
+  <div>
+    <div class="flex mt-3">
+      <h3>Config</h3>
+    </div>
+    {{ getConfig }}
+
+    <div class="flex mt-3">
+      <h3>Token</h3>
+    </div>
+
+    <div class="wrap-block">
+      {{ getAccessToken }}
+    </div>
+
+    <div class="flex mt-3">
+      <h3>Profile</h3>
+    </div>
+    {{ getProfile }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h3 {
+  margin-top: 1em;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Message } from '@/lib/primevue';
+import { storeToRefs } from 'pinia';
+
+import { useConfigStore } from '@/store';
+
+// Store
+const { getConfig } = storeToRefs(useConfigStore());
+
+</script>
+
+<template>
+  <Message
+    v-if="getConfig?.notificationBanner"
+    severity="warn"
+  >
+    {{ getConfig?.notificationBanner }}
+  </Message>
+
+  <div class="text-center">
+    <h1 class="font-bold">AI Form Assistant Showcase</h1>
+  </div>
+</template>
